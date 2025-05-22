202c4c41a4bef9ba7913c3001674b6ea89bdcc98
David Zhao <15309698+ntierontime@users.noreply.github.com>
2025-05-22 07:42:37 -0400

    Add Data Migration: AdventureWorksLT2019.InitialCreate, which is a faked migration, no database modification, as a start point after database reengineer.

---
+++ WebApi/Data/Migrations/AdventureWorksLT2019/20250522113758_AdventureWorksLT2019InitialCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdventureWorksLT2019.WebApi.Data.Migrations.AdventureWorksLT2019
+{
+    /// <inheritdoc />
+    public partial class AdventureWorksLT2019InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
